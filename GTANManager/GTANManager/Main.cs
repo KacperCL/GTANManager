b8e4853c7eaedfbe32c4c6a71e1975f568b2f2d5
vfrz <fritz.valentin@live.fr>
2017-01-12 21:19:54 +0100

    First upload

---
+++ GTANManager/GTANManager/Main.cs
@@ -0,0 +1,42 @@
+﻿using GTANetworkServer;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GTANManager
+{
+    public class Main : Script
+    {
+
+        private ManagerForm manager = null;
+
+        public Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            manager = new ManagerForm(API.getServerName(), API);
+
+            new Thread(() =>
+            {
+                Application.Run(manager);
+            }).Start();
+
+            
+
+            API.onPlayerConnected += API_onPlayerConnected;
+            API.onPlayerDisconnected += API_onPlayerDisconnected;
+        }
+
+        private void API_onPlayerDisconnected(Client player, string reason)
+        {
+            if (manager != null)
+                manager.RemovePlayer(player);
+        }
+
+        private void API_onPlayerConnected(Client player)
+        {
+            if (manager != null)
+                manager.AddPlayer(player);
+        }
+        
+    }
+}
---
+++ GTANManager/GTANManager/ManagerForm.cs
@@ -0,0 +1,141 @@
+﻿using GTANetworkServer;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace GTANManager
+{
+    public partial class ManagerForm : Form
+    {
+
+        private API API;
+        private List<Client> players;
+
+        public ManagerForm(string title, API api)
+        {
+            InitializeComponent();
+
+            API = api;
+
+            players = new List<Client>();
+
+            Text = "GTA:N Manager : " + title;
+
+            UpdateResourceLists();
+
+        }
+
+        private void UpdateResourceLists()
+        {
+            resourceList.Items.Clear();
+            resourceRunningList.Items.Clear();
+            foreach (var r in API.getAllResources())
+            {
+                if (r != "gtanmanager")
+                {
+                    if (API.isResourceRunning(r))
+                        resourceRunningList.Items.Add(r);
+                    else
+                        resourceList.Items.Add(r);
+                }
+            }
+        }
+
+        public void AddPlayer(Client client)
+        {
+            playerList.Items.Add(client.socialClubName);
+            players.Add(client);
+        }
+
+        public void RemovePlayer(Client client)
+        {
+            if (playerList.Items.Contains(client.socialClubName))
+                playerList.Items.Remove(client.socialClubName);
+            if (players.Contains(client))
+                players.Remove(client);
+        }
+
+        private void restartResourceButton_Click(object sender, EventArgs e)
+        {
+            if (resourceRunningList.SelectedIndex >= 0)
+            {
+                var r = resourceRunningList.SelectedItem.ToString();
+                if (API.isResourceRunning(r))
+                {
+                    API.stopResource(r);
+                    API.startResource(r);
+                }
+            }
+            UpdateResourceLists();
+        }
+
+        private void stopResourceButton_Click(object sender, EventArgs e)
+        {
+            if (resourceRunningList.SelectedIndex >= 0)
+            {
+                var r = resourceRunningList.SelectedItem.ToString();
+                if (API.isResourceRunning(r))
+                    API.stopResource(r);
+            }
+            UpdateResourceLists();
+        }
+
+        private void startResourceButton_Click(object sender, EventArgs e)
+        {
+            if (resourceList.SelectedIndex >= 0)
+            {
+                var r = resourceList.SelectedItem.ToString();
+                if (!API.isResourceRunning(r))
+                    API.startResource(r);
+            }
+            UpdateResourceLists();
+        }
+
+        private void kickButton_Click(object sender, EventArgs e)
+        {
+            if (playerList.SelectedIndex >= 0)
+            {
+                var p = playerList.SelectedItem.ToString();
+                foreach (var c in players)
+                {
+                    if (p == c.socialClubName)
+                    {
+                        var reason = Interaction.InputBox("Enter kick reason (1 space for no reason) :", "Kick reason", " ");
+                        if (reason != "")
+                            API.kickPlayer(c, reason);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void banButton_Click(object sender, EventArgs e)
+        {
+            if (playerList.SelectedIndex >= 0)
+            {
+                var p = playerList.SelectedItem.ToString();
+                foreach (var c in players)
+                {
+                    if (p == c.socialClubName)
+                    {
+                        var reason = Interaction.InputBox("Enter ban reason (1 space for no reason) :", "Ban reason", " ");
+                        if (reason != "")
+                            API.banPlayer(c, reason);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void kickAll_Click(object sender, EventArgs e)
+        {
+            var reason = Interaction.InputBox("Enter kick reason (1 space for no reason) :", "Kick reason", " ");
+            if (reason != "")
+                foreach (var c in players)
+                    API.kickPlayer(c, reason);
+        }
+
+    }
+
+}
